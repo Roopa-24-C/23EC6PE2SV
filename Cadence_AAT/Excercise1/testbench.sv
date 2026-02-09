94b4dc43af9fc5d2721e48c49df502cedc7f757c
Roopashri C <roopashric.ec23@bmsce.ac.in>
2026-02-09 19:52:46 +0530

    Update testbench.sv

--- Cadence_AAT/Excercise1/testbench.sv
+++ Cadence_AAT/Excercise1/testbench.sv
@@ -1,3 +1,11 @@
+// File        :tb_ALU.sv
+// Author      :Roopashri.C /1BM23EC218
+// Created     :09-02-2026
+// Module      :tb_ALU
+// Project     :SystemVerilog & Verification(23EC6PE2SV)
+// Faculty     :Prof.Ajaykumar Devarapalli
+// Description :This testbench applies different input combinations to verify all ALU operations.It also generates waveform data and runs random tests to check ALU functionality thoroughly.
+
 module ALU_tb(alu_if vif);
 
 initial begin
