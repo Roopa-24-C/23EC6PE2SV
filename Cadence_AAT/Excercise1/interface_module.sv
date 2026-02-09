549a2a4201e5823eac64cf57942bc0fbd8c1d616
Roopashri C <roopashric.ec23@bmsce.ac.in>
2026-02-09 19:54:23 +0530

    Update interface_module.sv

--- Cadence_AAT/Excercise1/interface_module.sv
+++ Cadence_AAT/Excercise1/interface_module.sv
@@ -1,3 +1,10 @@
+// File        :ALU_INTERFACE.sv
+// Author      :Roopashri.C /1BM23EC218
+// Created     :09-02-2026
+// Module      :alu_if
+// Project     :SystemVerilog & Verification(23EC6PE2SV)
+// Faculty     :Prof.Ajaykumar Devarapalli
+// Description :This interface groups ALU input and output signals into a single communication bundle.It simplifies connection between the ALU design and the testbench.
 interface alu_if;
     logic [31:0] a, b;
     logic [1:0] opcode;
