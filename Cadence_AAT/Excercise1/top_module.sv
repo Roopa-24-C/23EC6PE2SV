f886eafffea7a4f9b7a015612785075bae6e6aba
Roopashri C <roopashric.ec23@bmsce.ac.in>
2026-02-09 19:56:01 +0530

    Update top_module.sv

--- Cadence_AAT/Excercise1/top_module.sv
+++ Cadence_AAT/Excercise1/top_module.sv
@@ -1,3 +1,11 @@
+// File        :ALU_TOP_MODULE.sv
+// Author      :Roopashri.C /1BM23EC218
+// Created     :09-02-2026
+// Module      :Top
+// Project     :SystemVerilog & Verification(23EC6PE2SV)
+// Faculty     :Prof.Ajaykumar Devarapalli
+// Description :This top module connects the ALU design with the testbench using the interface.It acts as the main wrapper to run simulation and verify the ALU.
+
 module Top;
 
 alu_if vif();
