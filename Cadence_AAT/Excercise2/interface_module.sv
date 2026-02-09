bd305483660a6237c89c4dadb8ec6d79b2060bc9
Roopashri C <roopashric.ec23@bmsce.ac.in>
2026-02-09 19:59:36 +0530

    Update interface_module.sv

--- Cadence_AAT/Excercise2/interface_module.sv
+++ Cadence_AAT/Excercise2/interface_module.sv
@@ -1,3 +1,11 @@
+// File        :ATM_Interface.sv
+// Author      :Roopashri.C /1BM23EC218
+// Created     :09-02-2026
+// Module      :atm_if
+// Project     :SystemVerilog & Verification(23EC6PE2SV)
+// Faculty     :Prof.Ajaykumar Devarapalli
+// Description :This interface groups all ATM control and status signals into one connection bundle.It simplifies communication between the ATM module and its testbench.
+
 interface atm_if;
     logic clk;
     logic reset;
