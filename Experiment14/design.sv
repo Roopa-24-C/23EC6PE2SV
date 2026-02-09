166e241f3c7c6bd0dcc660a4c0c235062c93b635
Roopashri C <roopashric.ec23@bmsce.ac.in>
2026-02-09 21:30:09 +0530

    Update design.sv

--- Experiment14/design.sv
+++ Experiment14/design.sv
@@ -8,13 +8,18 @@
 class Packet;
   rand bit [7:0] data;
 
+  // Virtual method
   virtual function void print();
-    $display("Normal: %h", data);
+    $display("Normal Packet: %h", data);
   endfunction
 endclass
 
+
 class BadPacket extends Packet;
+
+  // Method override
   virtual function void print();
-    $display("ERROR: %h", data);
+    $display("ERROR Packet: %h", data);
   endfunction
+
 endclass
